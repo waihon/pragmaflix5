0a4e94276f3aef2f59882d43815ef5d156ffd1ed
Waihon Yew <yewwaihon@gmail.com>
2016-10-27 21:28:49 +0800

    Complete 10. Model

---
+++ db/migrate/20161027002959_add_fields_to_movie.rb
@@ -0,0 +1,6 @@
+class AddFieldsToMovie < ActiveRecord::Migration[5.0]
+  def change
+    add_column :movies, :description, :text
+    add_column :movies, :released_on, :date
+  end
+end
--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -3,12 +3,39 @@ require "rails_helper"
 describe "Viewing the list of movies" do
 
   it "shows the movies" do
+    # Arrange
+    movie1 = Movie.create(title: "Iron Man",
+                          rating: "PG-13",
+                          total_gross: 318_412_101.00,
+                          description: "Tony Stark builds an armored suit to fight the throes of evil",
+                          released_on: "2008-05-02")
+
+    movie2 = Movie.create(title: "Superman",
+                          rating: "PG",
+                          total_gross: 134218018.00,
+                          description: "Clark Kent grows up to be the greatest super-hero",
+                          released_on: "1978-12-15")
+
+    movie3 = Movie.create(title: "Spider-Man",
+                          rating: "PG-13",
+                          total_gross: 403706375.00,
+                          description: "Peter Parker gets bit by a genetically modified spider",
+                          released_on: "2002-05-03")
+
+    # Act
     visit movies_url
 
+    # Assert
     expect(page).to have_text("3 Movies")
-    expect(page).to have_text("Movie 1")
-    expect(page).to have_text("Movie 2")
-    expect(page).to have_text("Movie 3")
+    expect(page).to have_text(movie1.title)
+    expect(page).to have_text(movie2.title)
+    expect(page).to have_text(movie3.title)
+
+    expect(page).to have_text(movie1.rating)
+    expect(page).to have_text(movie1.description[0..9])
+    expect(page).to have_text(movie1.released_on)
+    # Number formatting helpers aren't available in specs.
+    expect(page).to have_text("$318,412,101.00")
   end
 
 end
