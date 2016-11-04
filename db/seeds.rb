f8b754ed151cd1fd33d0831947968243acde62f7
Waihon Yew <yewwaihon@gmail.com>
2016-11-04 21:08:56 +0800

    Complete 10. Check In Browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create!([
+  {
+    title: "Iron Man",
+    rating: "PG-13",
+    total_gross: 318_412_101.00,
+    description: "Tony Stark builds an armored suit to fight the throes of evil",
+    released_on: "2008-05-02"
+  },
+  {
+    title: "Superman",
+    rating: "PG",
+    total_gross: 134_218_018.00,
+    description: "Clark Kent grows up to be the greatest super-hero",
+    released_on: "1978-12-15"
+  },
+  {
+    title: "Spider-Man",
+    rating: "PG-13",
+    total_gross: 403_706_375.00,
+    description: "Peter Parker gets bit by a genetically modified spider",
+    released_on: "2002-05-03"
+  },
+  {
+    title: "Catwoman",
+    rating: "PG-13",
+    total_gross: 40_200_000.00,
+    description: "Patience Philips has a more than respectable career as a graphic designer",
+    released_on: "2004-07-23"
+  }
+])
