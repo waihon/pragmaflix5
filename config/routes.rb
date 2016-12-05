885e7af24dc9b61ae97cfc83358b978799bd1c4c
Waihon Yew <yewwaihon@gmail.com>
2016-12-05 19:47:14 +0800

    Complete 22. One-to-Many: Nested Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
   root "movies#index"
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 end
